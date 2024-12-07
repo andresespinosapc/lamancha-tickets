38517e99a3bb88fd39d2bdd07f073390be3c84a4
Andres Espinosa <andresespinosapc@gmail.com>
2024-12-07 17:13:05 -0300

    feat(ticket): add route to generate blank tickets

--- src/server/api/routers/ticket.ts
+++ src/server/api/routers/ticket.ts
@@ -4,7 +4,10 @@ import {
   createTRPCRouter,
   publicProcedure,
 } from "~/server/api/trpc";
+import { EmailService } from "~/server/services/email";
 import { TicketService } from "~/server/services/ticket";
+import { env } from "~/env";
+import { HashidService } from "~/server/services/hashid";
 
 export const ticketRouter = createTRPCRouter({
   generateTickets: publicProcedure.input(z.object({
@@ -46,5 +49,41 @@ export const ticketRouter = createTRPCRouter({
         })
       }))
     })
+  }),
+  generateBlankTickets: publicProcedure.input(z.object({
+    tickets: z.array(z.object({
+      ticketTypeId: z.number(),
+      attendee: z.object({
+        email: z.string().email(),
+      }),
+    }))
+  })).mutation(async ({ ctx, input }) => {
+    const newTickets = await ctx.db.$transaction(async tx => {
+      return Promise.all(input.tickets.map(async ticketData => {
+        return tx.ticket.create({
+          data: {
+            attendee: {
+              create: ticketData.attendee
+            },
+            ticketType: {
+              connect: {
+                id: ticketData.ticketTypeId,
+              }
+            }
+          },
+          include: {
+            attendee: true,
+          }
+        })
+      }))
+    })
+    await Promise.all(newTickets.map(async ticket => {
+      const completeTicketUrl = `${env.FRONTEND_BASE_URL}/tickets/${ticket.hashid}/complete`;
+      return new EmailService().sendMail({
+        to: ticket.attendee.email,
+        subject: 'Completa tus datos',
+        text: `Hola, por favor completa tus datos en el siguiente link para generar tu ticket: ${completeTicketUrl}`,
+      });
+    }))
   }),
 });
