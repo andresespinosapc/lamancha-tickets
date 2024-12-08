a0f6623a00f6655beed1d2392969e29e6fee7c84
Andres Espinosa <andresespinosapc@gmail.com>
2024-12-08 00:21:07 -0300

    feat(auth): setup credentials provider

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,8 +1,9 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import { db } from "~/server/db";
+import { compareSync } from "bcrypt";
+import { z } from "zod";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -32,25 +33,31 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
-  adapter: PrismaAdapter(db),
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "juanito@gmail.com" },
+        password: { label: "Contraseña", type: "password" }
       },
-    }),
-  },
+      async authorize(credentials, _req) {
+        const parsedCredentials = z.object({
+          email: z.string(),
+          password: z.string(),
+        }).parse(credentials);
+
+        const user = await db.user.findFirst({
+          where: {
+            email: parsedCredentials.email,
+          },
+        });
+
+        if (!user) return null;
+        if (!user.password) return null;
+
+        if (!compareSync(parsedCredentials.password, user.password)) return null;
+
+        return user;
+      }
+    })
+  ],
 } satisfies NextAuthConfig;
