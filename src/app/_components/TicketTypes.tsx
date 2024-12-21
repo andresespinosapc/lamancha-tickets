1eadf3829c358a15a76103187360f5168508f31b
Andres Espinosa <andresespinosapc@gmail.com>
2024-12-21 00:49:28 -0300

    feat(ticket-types): sort by price

--- src/app/_components/TicketTypes.tsx
+++ src/app/_components/TicketTypes.tsx
@@ -11,7 +11,7 @@ export function TicketTypes() {
     <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
       <h1 className="text-2xl font-bold mb-6 text-center">Entradas disponibles</h1>
       <ul className="space-y-4">
-        {ticketTypes.map((ticket) => (
+        {ticketTypes.sort((a, b) => a.price - b.price).map((ticket) => (
           <li
             key={ticket.id}
             className="flex items-center justify-between p-4 bg-gray-100 rounded-md"
